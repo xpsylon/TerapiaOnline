3a7b6bd3fd1d2ee48b0a0440746ba09c72882a31
xpsylon <xxxpsylon@gmail.com>
2023-08-28 19:57:14 +0200

    added crispy and bootstrap to settings

--- TerapiaOnline/settings.py
+++ TerapiaOnline/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'therapists.apps.TherapistsConfig',
     'users.apps.UsersConfig', 
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
