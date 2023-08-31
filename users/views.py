c11c6fb05e25a73085bae9e805228769d83247ad
xpsylon <xxxpsylon@gmail.com>
2023-08-31 12:48:15 +0200

    just before creating an experimental branch

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Hola que tul, men</h1>')
+    return render(request, 'users/home.html')
 
 def base(request):
     return render(request, 'users/base.html')
