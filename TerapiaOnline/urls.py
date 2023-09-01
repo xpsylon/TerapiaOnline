cbef254a6c576e053f8be4588e84eb4f977dd04d
xpsylon <xxxpsylon@gmail.com>
2023-09-01 15:03:58 +0200

    home url now in main/urls

--- TerapiaOnline/urls.py
+++ TerapiaOnline/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from main import views as main_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', main_views.home , name='home'),
+    path('', include('main.urls')),
 ]
