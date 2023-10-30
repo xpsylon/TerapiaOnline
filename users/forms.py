55885d315f5786f9c56e64d100025e1bb9fa7212
xpsylon <xxxpsylon@gmail.com>
2023-10-30 17:06:09 +0100

    users and so on despues de mucho tiempo

--- users/forms.py
+++ users/forms.py
@@ -4,8 +4,8 @@ from django import forms
 from django.contrib.auth.models import User
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+    email = forms.EmailField() #agrega campo email
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2'] #y pasa estos campos al user recien creado.
