a13f861253f5304cf8e3d6b789bf34acf6769a32
xpsylon <xxxpsylon@gmail.com>
2023-10-31 18:10:02 +0100

    until 60 mins video techtim

--- users/views.py
+++ users/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import UserRegisterForm
+from django.contrib.auth import login
 
 def registro(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            #guarda los datos del form en variable user:
+            user = form.save()
+            #aparte loginea al user recien creado (previamente hay que importar el metodo login)
+            #en Project Three es diferente, se crea perfil y el redirect es a login para obviamente loginearse:
+            login(request, user)
+            return redirect('main:casa')
     else:
         form = UserRegisterForm()
 
