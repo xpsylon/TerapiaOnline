83c7bda56250a07744334e9e7da27f3ec124d6f4
xpsylon <xxxpsylon@gmail.com>
2023-11-03 18:05:11 +0100

    form for post added

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    creado = models.DateTimeField(auto_now_add=True)
+    actualizado = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.titulo + '/n' + self.descripcion
+    
+
