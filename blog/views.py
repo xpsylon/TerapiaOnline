75a4071cb2df183cd8e270ce33dd216447a7a8fd
xpsylon <xxxpsylon@gmail.com>
2023-11-21 15:46:28 +0100

    updated post creation form

--- blog/views.py
+++ blog/views.py
@@ -16,3 +16,18 @@ def nuevo_posteo(request):
         form = PostForm()
     return render(request, 'blog/post_form.html', {'formulario':form})
 
+"""
+    This view function handles the creation of a new post.
+
+    The function is decorated with the `login_required` decorator, which ensures that only authenticated users can access this view.
+
+    If the request method is 'POST', it means the form is being submitted. The function then checks if the form is valid. If it is, it saves the form but with commit=False to prevent it from being saved immediately. This is done so that the author of the post can be added before saving the post to the database.
+
+    If the form is not valid or if the request method is not 'POST' (which means the form is being accessed for the first time), an empty form is created and passed to the template.
+
+    Parameters:
+    request (WSGIRequest): A HTTP Request instance.
+
+    Returns:
+    HTTPResponse: The HTTP Response instance.
+    """
