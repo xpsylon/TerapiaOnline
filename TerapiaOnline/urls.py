5e7bf9920f89deda67c7e4c137520266f9665dfd
xpsylon <xxxpsylon@gmail.com>
2023-11-24 15:58:41 +0100

    deleted signal tests. added signals.py

--- TerapiaOnline/urls.py
+++ TerapiaOnline/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+#estas dos proximas lineas son para servir media files...chequear linea + static ...
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,7 +27,7 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('', include('users.urls', namespace='users')),
     path('blog/', include('blog.urls', namespace='blog'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 """
 URL Configuration for the Django project.
---
+++ users/signals.py
@@ -0,0 +1,35 @@
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from .models import Profile
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+        
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
+    
+    """ 1. `from django.db.models.signals import post_save`: This line is importing the `post_save` signal from Django. This signal is sent at the end of the `save()` method of a model. The signal is provided with the sender (the model class), instance (the actual instance being saved), created (a boolean indicating whether a new record was created), and many other keyword arguments.
+
+2. `from django.contrib.auth.models import User`: This line is importing Django's built-in User model. The User model is the sender of the signal. In other words, the signal will be sent whenever a User instance is saved.
+
+3. `from django.dispatch import receiver`: This line is importing the `receiver` decorator from Django. This decorator is used to specify that a certain function is a receiver function for a certain signal.
+
+4. `from .models import Profile`: This line is importing the Profile model from the same directory. The Profile model is presumably a model that represents user profiles.
+
+5. `@receiver(post_save, sender=User)`: This line is using the `receiver` decorator to specify that the following function is a receiver function for the `post_save` signal sent by the User model.
+
+6. `def create_profile(sender, instance, created, **kwargs):`: This function is defined as a receiver function for the `post_save` signal sent by the User model. It takes four arguments: the sender of the signal, the instance that was saved, a boolean indicating whether a new record was created, and any additional keyword arguments.
+
+7. `if created: Profile.objects.create(user=instance)`: This line is checking whether a new record was created. If it was, it creates a new Profile instance associated with the User instance that was saved.
+
+8. `def save_profile(sender, instance, **kwargs):`: This function is another receiver function for the `post_save` signal sent by the User model. It takes three arguments: the sender of the signal, the instance that was saved, and any additional keyword arguments.
+
+9. `instance.profile.save()`: This line saves the Profile instance associated with the User instance that was saved.
+
+In summary, this code automatically creates and saves a Profile instance whenever a new User instance is created. This is a common pattern in Django for creating related records at the same time as a primary record. For example, you might use this pattern to automatically create a user profile, a shopping cart, or other related records when a new user signs up for your site. 
+
+I hope this helps! Let me know if you have any other questions. 😊 """
