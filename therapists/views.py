4cb0d9a2ff340985b9de56439e3d6a913632d754
xpsylon <xxxpsylon@gmail.com>
2023-12-17 11:38:07 +0100

    therapists alfa

--- therapists/views.py
+++ therapists/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Therapist
 
 def therapist_list(request):
     therapists = Therapist.objects.all()
     return render(request, 'therapists/therapist_list.html', {'terapeutas':therapists})
 
-def therapist_detail(request):
-    therapist = Therapist.objects.get(id=therapist.id)
-    return render(request, 'therapist_detail.html', {'terapeuta':therapist})
+def therapist_detail(request, therapist_id):
+    therapist = get_object_or_404(Therapist, id=therapist_id)
+    return render(request, 'therapists/therapist_detail.html', {'terapeuta':therapist})
