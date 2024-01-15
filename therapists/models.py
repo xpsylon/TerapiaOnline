05420d9de7ccbb79ebd355f6fcaa4fe8b5ddb891
xpsylon <xxxpsylon@gmail.com>
2024-01-15 11:48:11 +0100

    updating for restarting

--- therapists/models.py
+++ therapists/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 class Therapist(models.Model):
     name = models.CharField(max_length=100)
