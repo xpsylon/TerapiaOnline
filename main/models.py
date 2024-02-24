3a345fc9fc3b2b19a9d81e53c1a15de1c94fa722
xpsylon <xxxpsylon@gmail.com>
2024-02-24 17:39:49 +0100

    main/models/lookup tables

--- main/models.py
+++ main/models.py
@@ -1,1 +1,9 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
+
+class Country(models.Model):
+    name = models.Model(max_length=50)
+
+class City(models.Model):
+    name = models.Model(max_lenght=50)
+    
